dcf52af59e1eb814fed64d4b9346eabf281ab804
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-08-19 11:12:38 +0200

    added logic for location generation

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -1,9 +1,24 @@
 import ListItem from "../ListItem/ListItem";
+import locations from "../../data/locations.json";
 
 export default function LocationList() {
+  const randomLocations = [...locations]
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 4);
+
+  // Add logic to save visited locations and exclude them from future lists
+
   return (
     <div>
-      <ListItem />
+      {randomLocations.map((location, index) => (
+        <ListItem
+          key={index}
+          name={location.name}
+          description={location.description}
+          longitude={location.longitude}
+          latitude={location.latitude}
+        />
+      ))}
     </div>
   );
 }
