12a1e11f6337d1927d2eddd15341e44c8dcc4324
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-08-19 16:41:55 +0200

    changed props to match new data file

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,12 +1,13 @@
 import "./ListItem.css";
 
-export default function ListItem({ name, description, longitude, latitude }) {
+export default function ListItem({ name, description, distance, exercise }) {
   return (
     <div className="list-item-container">
-      <p className="title">Name: {name}</p>
-      <p className="description">Description: {description}:</p>
-      <p className="longitude">Longitude: {longitude}</p>
-      <p className="latitude">Latitude: {latitude}</p>
+      <p className="title">Namn: {name}</p>
+      <p className="description">{description}</p>
+      <p className="distance">Avstånd: {distance.toFixed(0)} m</p>
+      <p className="exercise-title">Övning: {exercise.title}</p>
+      <p className="exercise-instructions">{exercise.instructions}</p>
     </div>
   );
 }
