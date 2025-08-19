1be7a7d1166ccb4e0e1bf1096774d143bdf0c23e
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-08-19 17:58:46 +0200

    added user location logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Compass from "./components/Compass/Compass";
 import LocationList from "./components/LocationList/LocationList";
 
 function App() {
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error(error);
+          setUserLocation({ lat: 57.705896, lng: 11.936357 }); // Fallback position
+        }
+      );
+    }
+  }, []);
+
+  if (!userLocation) return <p>Hämtar position…</p>;
+
   return (
     <>
       <Compass />
-      <LocationList userLocation={{ lat: 57.705, lng: 11.937 }} />
+      <LocationList userLocation={userLocation} />
     </>
   );
 }
