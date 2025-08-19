33dcde7585c6588f58987a176502876ff8e461b4
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-08-19 17:18:51 +0200

    added artist

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,9 +1,16 @@
 import "./ListItem.css";
 
-export default function ListItem({ name, description, distance, exercise }) {
+export default function ListItem({
+  name,
+  artist,
+  description,
+  distance,
+  exercise,
+}) {
   return (
     <div className="list-item-container">
       <p className="title">Namn: {name}</p>
+      <p className="artist">Konstnär: {artist}</p>
       <p className="description">{description}</p>
       <p className="distance">Avstånd: {distance.toFixed(0)} m</p>
       <p className="exercise-title">Övning: {exercise.title}</p>
