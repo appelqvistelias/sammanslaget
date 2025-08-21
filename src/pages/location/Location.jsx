3dae7e19ea226d855cf29a2395e08fefdff610d6
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-08-21 11:01:22 +0200

    started building location page

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -1,1 +1,29 @@
-export default function Location() {}
+import style from "./Location.module.css";
+import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
+
+export default function Location() {
+  return (
+    <main className={style.main}>
+      <h1 className={style.mainHeader}>Namn på platsen</h1>
+      <p className={style.artist}>Årtal, Konstnär</p>
+      <div className={style.imageSection}>
+        <img />
+        <PrimaryButton textContent={"Starta aktivitet"} />
+      </div>
+      <div className={style.activitySection}>
+        <div className={style.activityTextContainer}>
+          <h2 className={style.activityHeader}>Aktivitet</h2>
+          <p className={style.activityText}>Beskrivning av aktivteten</p>
+        </div>
+        <PrimaryButton textContent={"Gå vidare"} />
+      </div>
+      <div className={style.about}>
+        <h3 className={style.secondaryHeader}>Information om platsen</h3>
+        <hr className={style.ruler} />
+        <div className={style.aboutTextContainer}>
+          <p className={style.aboutText}>Text om platsen</p>
+        </div>
+      </div>
+    </main>
+  );
+}
