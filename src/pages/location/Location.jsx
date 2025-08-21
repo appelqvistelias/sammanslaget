949d1a47cc29903bf20019954331df50156b18b4
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-08-21 11:18:01 +0200

    styling page

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -4,24 +4,26 @@ import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
 export default function Location() {
   return (
     <main className={style.main}>
-      <h1 className={style.mainHeader}>Namn på platsen</h1>
-      <p className={style.artist}>Årtal, Konstnär</p>
-      <div className={style.imageSection}>
-        <img />
-        <PrimaryButton textContent={"Starta aktivitet"} />
-      </div>
-      <div className={style.activitySection}>
-        <div className={style.activityTextContainer}>
-          <h2 className={style.activityHeader}>Aktivitet</h2>
-          <p className={style.activityText}>Beskrivning av aktivteten</p>
+      <div className={style.wrapper}>
+        <h1 className={style.mainHeader}>Namn på platsen</h1>
+        <div className={style.imageSection}>
+          <p className={style.artist}>Årtal, Konstnär</p>
+          <img />
+          <PrimaryButton textContent={"Starta aktivitet"} />
         </div>
-        <PrimaryButton textContent={"Gå vidare"} />
-      </div>
-      <div className={style.about}>
-        <h3 className={style.secondaryHeader}>Information om platsen</h3>
-        <hr className={style.ruler} />
-        <div className={style.aboutTextContainer}>
-          <p className={style.aboutText}>Text om platsen</p>
+        <div className={style.activitySection}>
+          <div className={style.activityTextContainer}>
+            <h2 className={style.activityHeader}>Aktivitet</h2>
+            <p className={style.activityText}>Beskrivning av aktivteten</p>
+          </div>
+          <PrimaryButton textContent={"Gå vidare"} />
+        </div>
+        <div className={style.about}>
+          <h3 className={style.aboutHeader}>Information om platsen</h3>
+          <hr className={style.ruler} />
+          <div className={style.aboutTextContainer}>
+            <p className={style.aboutText}>Text om platsen</p>
+          </div>
         </div>
       </div>
     </main>
